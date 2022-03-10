31d577d2ffc8710834a31eaa680f5a8cbf94e7aa
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-03-10 16:21:29 +0000

    Remove duplicate in urls.py

--- config/urls.py
+++ config/urls.py
@@ -36,7 +36,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
     # Your stuff: custom urls includes go here
-    path("accounts/", include("allauth.urls")),
     path("", include("judgments.urls")),
     path("judgments_admin/", include("judgments_admin.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
