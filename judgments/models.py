aa56086ae931ebf119baa4677fd4676a3f1bbe4c
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-04-21 13:48:55 +0100

    Add submission information to the recent judgments listing

--- judgments/models.py
+++ judgments/models.py
@@ -1,9 +1,27 @@
 # from django.db import models
+from datetime import datetime
 from os.path import dirname, join
 
 from caselawclient.Client import api_client
 from djxml import xmlmodels
 from lxml import etree
+
+
+class SearchResultMeta:
+    def __init__(self, author="", author_email="", consignment_reference="", submission_datetime=""):
+        self.author = author
+        self.author_email = author_email
+        self.consignment_reference = consignment_reference
+        self.submission_datetime = datetime.strptime(submission_datetime, "%Y-%m-%dT%H:%M:%SZ") if submission_datetime else datetime.min
+
+    @staticmethod
+    def create_from_uri(uri: str):
+        return SearchResultMeta(
+            author=api_client.get_property(uri, "source-name"),
+            author_email=api_client.get_property(uri, "source-email"),
+            consignment_reference=api_client.get_property(uri, "transfer-consignment-reference"),
+            submission_datetime=api_client.get_property(uri, "transfer-received-at")
+        )
 
 
 class Judgment(xmlmodels.XmlModel):
@@ -27,7 +45,7 @@ class Judgment(xmlmodels.XmlModel):
 
 class SearchResult:
     def __init__(
-        self, uri="", neutral_citation="", name="", court="", date="", matches=[]
+        self, uri="", neutral_citation="", name="", court="", date="", matches=[], meta=SearchResultMeta()
     ) -> None:
         self.uri = uri
         self.neutral_citation = neutral_citation
@@ -35,6 +53,7 @@ class SearchResult:
         self.date = date
         self.court = court
         self.matches = matches
+        self.meta = meta
 
     @staticmethod
     def create_from_node(node):
@@ -44,6 +63,7 @@ class SearchResult:
         )
         judgment_xml = api_client.get_judgment_xml(uri, show_unpublished=True)
         judgment = Judgment.create_from_string(judgment_xml)
+
         return SearchResult(
             uri=uri,
             neutral_citation=judgment.neutral_citation,
@@ -51,6 +71,7 @@ class SearchResult:
             matches=matches.transform_to_html(),
             court=judgment.court,
             date=judgment.date,
+            meta=SearchResultMeta.create_from_uri(uri)
         )
 
 
