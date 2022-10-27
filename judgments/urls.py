f51bd450823630f32642a5cd0e4a4f5868965e78
David McKee <dragon@dxw.com>
2022-10-27 17:42:48 +0100

    Editor priorities selection
    Doesn't feature actually showing the priority. Sorting is next.

--- judgments/urls.py
+++ judgments/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("detail", views.detail, name="detail"),
     path("xml", views.detail_xml, name="detail_xml"),
     path("results", views.results, name="results"),
+    # buttons that do a thing and redirect
     path("delete", views.delete, name="delete"),
     path("assign", views.assign_judgment_button, name="assign"),
+    path("prioritise", views.prioritise_judgment_button, name="prioritise"),
     path("", views.index, name="home"),
 ]
