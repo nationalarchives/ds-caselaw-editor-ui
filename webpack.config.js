8097ef7b3856df7887c0a74d263fe7e3d462d30e
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-12-19 09:35:58 +0100

    Add GTM tags and nationalarchives cookie consent banner
    This follows the pattern set in the public UI, including the standard NA cookie banner,
    and a django context processor to selectively include the GTM tags based on whether the cookie
    is set and consnet given.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devtool: false,
-  entry: './ds_caselaw_editor_ui/static/js/src/app.js',
+  entry: {
+    app: "./ds_caselaw_editor_ui/static/js/src/app.js",
+    cookie_consent:
+      "./ds_caselaw_editor_ui/static/js/cookie_consent/src/ds-cookie-consent.js",
+    gtm_script: "./ds_caselaw_editor_ui/static/js/src/gtm_script.js",
+  },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'ds_caselaw_editor_ui/static/js/dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "ds_caselaw_editor_ui/static/js/dist"),
   },
   module: {
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }
-  ]
-}
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
