f953cf0d57d2fff6f51250239be6d8a890cef346
Nick Jackson <nick@dxw.com>
2023-05-23 16:27:06 +0100

    Add prettier to pre-commit
    This runs for MarkDown, YAML, SCSS, JavaScript and XML files to make them beautiful and consistent.

--- ds_caselaw_editor_ui/static/js/cookie_consent/src/data.js
+++ ds_caselaw_editor_ui/static/js/cookie_consent/src/data.js
@@ -33,20 +33,24 @@ const Data = {
   cookies: {
     cookieOne: "dontShowCookieNotice",
     cookieTwo: "cookies_policy",
-    gaCookies: ["_ga", "_gid", "_gat_UA-2827241-22", "_gat_UA-2827241-1", "_ga_2CP7QT8TDG"],
-    settings: ["dontAutoStartResultsTour"]
+    gaCookies: [
+      "_ga",
+      "_gid",
+      "_gat_UA-2827241-22",
+      "_gat_UA-2827241-1",
+      "_ga_2CP7QT8TDG",
+    ],
+    settings: ["dontAutoStartResultsTour"],
   },
   formWrapper: {
     id: "#ds-cookie-consent-form",
   },
   acceptMessageAfterInteraction: {
-    text:
-      "You have accepted optional cookies. You can change your cookie settings on the <a href='https://www.nationalarchives.gov.uk/legal/cookies/'>Cookies page</a>.",
+    text: "You have accepted optional cookies. You can change your cookie settings on the <a href='https://www.nationalarchives.gov.uk/legal/cookies/'>Cookies page</a>.",
     ariaLabel: "Cookie consent confirmation message",
   },
   rejectMessageAfterInteraction: {
-    text:
-      "You have rejected optional cookies. You can change your cookie settings on the <a href='https://www.nationalarchives.gov.uk/legal/cookies/'>Cookies page</a>.",
+    text: "You have rejected optional cookies. You can change your cookie settings on the <a href='https://www.nationalarchives.gov.uk/legal/cookies/'>Cookies page</a>.",
     ariaLabel: "Cookie consent confirmation message",
   },
   oldCookieBannerWrapper: {
@@ -57,7 +61,7 @@ const Data = {
     two: "_gid",
     three: "_gat_UA-2827241-1",
     four: "_gat_UA-2827241-22",
-    five: "_ga_2CP7QT8TDG"
+    five: "_ga_2CP7QT8TDG",
   },
   DOM: {
     on: ".jsON",
@@ -70,8 +74,8 @@ const Data = {
     },
     settings: {
       rememberSettings: "#remember_your_settings",
-      doNotRememberSettings: "#donot_remember_your_settings"
-    }
+      doNotRememberSettings: "#donot_remember_your_settings",
+    },
   },
 };
 
