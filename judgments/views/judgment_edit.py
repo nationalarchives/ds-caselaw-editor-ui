287d0d3b5611bc7262ec3930407f531d1a0e4e9b
Nick Jackson <nick@dxw.com>
2023-06-21 12:56:47 +0100

    Remove ability to update NCN using the edit form
    The only path to update the NCN is now via the "Change neutral citation number" workflow, so we're not maintaining multiple code paths.

--- judgments/views/judgment_edit.py
+++ judgments/views/judgment_edit.py
@@ -2,17 +2,11 @@ import ds_caselaw_utils as caselawutils
 from caselawclient.Client import MarklogicAPIError, api_client
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
 from django.template import loader
 from django.urls import reverse
 from django.views.generic import View
 
-from judgments.utils import (
-    MoveJudgmentError,
-    NeutralCitationToUriError,
-    editors_dict,
-    update_document_uri,
-)
+from judgments.utils import editors_dict
 from judgments.utils.aws import invalidate_caches
 from judgments.utils.link_generators import (
     build_confirmation_email_link,
@@ -67,10 +61,6 @@ class EditJudgmentView(View):
             new_name = request.POST["metadata_name"]
             api_client.set_judgment_name(judgment_uri, new_name)
 
-            # Set neutral citation
-            new_citation = request.POST["neutral_citation"]
-            api_client.set_judgment_citation(judgment_uri, new_citation)
-
             # Set court
             new_court = request.POST["court"]
             api_client.set_judgment_court(judgment_uri, new_court)
@@ -93,20 +83,7 @@ class EditJudgmentView(View):
                 if new_assignment := request.POST.get("assigned_to", False):
                     api_client.set_property(judgment_uri, "assigned-to", new_assignment)
 
-            # If judgment_uri is a `failure` URI, amend it to match new neutral citation and redirect
-            if "failures" in judgment_uri and new_citation is not None:
-                new_judgment_uri = update_document_uri(judgment_uri, new_citation)
-                return redirect(
-                    reverse("edit-judgment", kwargs={"judgment_uri": new_judgment_uri})
-                )
-
             messages.success(request, "Judgment successfully updated")
-
-        except (MoveJudgmentError, NeutralCitationToUriError) as e:
-            messages.error(
-                request,
-                f"There was an error updating the Judgment's neutral citation: {e}",
-            )
 
         except MarklogicAPIError as e:
             messages.error(request, f"There was an error saving the Judgment: {e}")
