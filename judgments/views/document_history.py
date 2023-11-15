4a3eaaf74a07c1ed81c71fa9b25d2e71cf14563f
Nick Jackson <nick@dxw.com>
2023-11-15 13:15:25 +0000

    Add type checking to history structuring

--- judgments/views/document_history.py
+++ judgments/views/document_history.py
@@ -1,16 +1,58 @@
 import json
+from datetime import datetime
+from typing import Any, Literal, NotRequired, TypedDict
 
 from caselawclient.client_helpers import VersionType
+from caselawclient.models.documents import Document
 
 from judgments.utils.view_helpers import DocumentView
 
+VersionAnnotationDict = dict[str, Any]
 
-def structure_document_history_by_submissions(history):
+
+class EventDict(TypedDict):
+    data: VersionAnnotationDict
+    datetime: datetime
+    document: Document
+    marklogic_version: int
+    sequence_number: int
+    agent: NotRequired[str]
+    agent_email: NotRequired[str]
+
+
+class SubmissionDict(TypedDict):
+    pass
+
+
+class StructuredSubmissionDict(SubmissionDict):
+    submission_type: Literal["structured"]
+    submission_data: VersionAnnotationDict
+    document: Document
+    events: list[EventDict]
+    marklogic_version: int
+    sequence_number: int
+
+
+class OrphanSubmissionDict(SubmissionDict):
+    submission_type: Literal["orphan"]
+    events: list[EventDict]
+
+
+class LegacySubmissionDict(SubmissionDict):
+    submission_type: Literal["legacy"]
+    annotation: str
+    marklogic_version: int
+    sequence_number: int
+
+
+def structure_document_history_by_submissions(
+    history: list[Document],
+) -> list[SubmissionDict]:
     # An empty stack of submissions to begin with
-    structured_history: list = []
+    structured_history: list[SubmissionDict] = []
 
     # Start from the point of there being no valid submission in progress
-    submission = None
+    submission: SubmissionDict | None = None
 
     # Event sequence number increments on each new event
     event_sequence_number: int = 0
@@ -37,27 +79,31 @@ def structure_document_history_by_submissions(history):
 
                 submission_sequence_number += 1
 
-                submission = {
-                    "type": "structured",
-                    "submission_data": structured_data,
-                    "document": version,
-                    "events": [],
-                    "sequence_number": submission_sequence_number,
-                }
+                submission = StructuredSubmissionDict(
+                    {
+                        "submission_type": "structured",
+                        "submission_data": structured_data,
+                        "document": version,
+                        "events": [],
+                        "sequence_number": submission_sequence_number,
+                        "marklogic_version": version.version_number,
+                    },
+                )
 
             elif not submission:
                 # This isn't a submission, it's an event causing a new version. There's also a possibility that the
                 # first structured event we see happens outside a submission, in which case it gets its own special
                 # orphan submission.
-                submission = {
-                    "type": "structured",
-                    "orphan": True,
-                    "events": [],
-                }
+                submission = OrphanSubmissionDict(
+                    {
+                        "submission_type": "orphan",
+                        "events": [],
+                    },
+                )
 
             # At this point we've started a new submission if we need to, so go ahead and assemble the event object
             # and push it into the submission's event stack
-            event_object = {
+            event_object: EventDict = {
                 "data": structured_data,
                 "datetime": version.version_created_datetime,
                 "document": version,
@@ -90,12 +136,14 @@ def structure_document_history_by_submissions(history):
             # since this will upset the next loop.
             submission_sequence_number += 1
             structured_history.append(
-                {
-                    "type": "legacy",
-                    "annotation": version.annotation,
-                    "marklogic_version": version.version_number,
-                    "sequence_number": submission_sequence_number,
-                },
+                LegacySubmissionDict(
+                    {
+                        "submission_type": "legacy",
+                        "annotation": version.annotation,
+                        "marklogic_version": version.version_number,
+                        "sequence_number": submission_sequence_number,
+                    },
+                ),
             )
 
     # We're now done with the version stack. If a submission is still open, pop it on the stack.
@@ -111,7 +159,7 @@ class DocumentHistoryView(DocumentView):
 
         # The versions _usually_ come back from MarkLogic in an inverse list, but it's not guaranteed.
         # Forcibly put them in an ordered list so we can neatly process them.
-        sorted_ml_versions = sorted(
+        sorted_ml_versions: list[Document] = sorted(
             context["document"].versions_as_documents,
             key=lambda x: x.version_number,
         )
