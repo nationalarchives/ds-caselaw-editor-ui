83b594830c4fc0da65d6dee7d0d4142c5bbcfbdf
Nick Jackson <nick@dxw.com>
2023-11-15 17:03:56 +0000

    Add smoke test for structured history

--- judgments/tests/test_document_history.py
+++ judgments/tests/test_document_history.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from factories import JudgmentFactory
+from waffle.testutils import override_flag
 
 
 class TestDocumentHistory(TestCase):
@@ -40,3 +41,40 @@ class TestDocumentHistory(TestCase):
         assert dt.strftime("%d %b %Y") in decoded_response
         assert dt.strftime("%H:%M") in decoded_response
         assert "Submitted" in decoded_response
+
+
+@override_flag("history_timeline", active=True)
+class TestStructuredDocumentHistory(TestCase):
+    @patch("judgments.utils.view_helpers.get_document_by_uri_or_404")
+    @patch("judgments.utils.api_client.document_exists")
+    @patch("judgments.utils.api_client.get_document_type_from_uri")
+    def test_structured_history_flag(
+        self,
+        document_type,
+        document_exists,
+        mock_document,
+    ):
+        document_type.return_value = Judgment
+        document_exists.return_value = None
+
+        document = JudgmentFactory.build(
+            uri="edtest/4321/123",
+            name="Test v Tested",
+        )
+        mock_document.return_value = document
+
+        self.client.force_login(User.objects.get_or_create(username="testuser")[0])
+
+        assert (
+            reverse("document-history", kwargs={"document_uri": document.uri})
+            == "/edtest/4321/123/history"
+        )
+
+        response = self.client.get(
+            reverse("document-history", kwargs={"document_uri": document.uri}),
+        )
+
+        assert response.status_code == 200
+
+        decoded_response = response.content.decode("utf-8")
+        assert "Document history" in decoded_response
