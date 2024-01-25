009fc4c3f2489c18b96d8c1467d683dde907ece9
Nick Jackson <nick@dxw.com>
2024-01-25 10:57:31 +0000

    Add new report for documents awaiting re-parsing

--- judgments/views/reports.py
+++ judgments/views/reports.py
@@ -14,6 +14,25 @@ class Index(TemplateView):
         return context
 
 
+class AwaitingParse(TemplateView):
+    template_name = "reports/awaiting_parse.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        target_parser_version = api_client.get_highest_parser_version()
+
+        context["page_title"] = "Documents awaiting parsing"
+        context[
+            "target_parser_version"
+        ] = f"{target_parser_version[0]}.{target_parser_version[1]}"
+        context["documents"] = api_client.get_pending_parse_for_version(
+            target_parser_version,
+        )[1:]
+
+        return context
+
+
 class AwaitingEnrichment(TemplateView):
     template_name = "reports/awaiting_enrichment.html"
 
