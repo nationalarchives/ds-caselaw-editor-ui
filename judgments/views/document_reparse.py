d7a47a0dc344986e539474f8f2b20da09d85e25d
Dragon Dave McKee <david.mckee@dxw.com>
2024-03-07 15:43:11 +0000

    Don't assume reparse successful

--- judgments/views/document_reparse.py
+++ judgments/views/document_reparse.py
@@ -8,8 +8,11 @@ from judgments.utils.view_helpers import get_document_by_uri_or_404
 def reparse(request):
     document_uri = request.POST.get("document_uri")
     document = get_document_by_uri_or_404(document_uri)
-    document.reparse()
-    messages.success(request, f"Reparsing requested for {document.name}")
+    successfully_sent_for_reparsing = document.reparse()
+    if successfully_sent_for_reparsing:
+        messages.success(request, f"Reparsing requested for {document.name}")
+    else:
+        messages.error(request, f"Unable to reparse {document.name}. Not submitted.")
     return HttpResponseRedirect(
         reverse("full-text-html", kwargs={"document_uri": document.uri}),
     )
