46ccbcdee6b760b3b8ca34d2a3c2d2e930ac81ca
James Harries <dxw@pancakeuprising.com>
2024-07-26 11:29:51 +0100

    FCL-161 | add sidebar to styleguide

--- judgments/views/style_guide.py
+++ judgments/views/style_guide.py
@@ -27,6 +27,7 @@ class StyleGuide(TemplateView):
                     {"label": "Note", "href": "#note"},
                     {"label": "Notification messaging", "href": "#notification-messaging"},
                     {"label": "Search form", "href": "#search-form"},
+                    {"label": "Sidebar", "href": "#sidebar"},
                     {"label": "Summary panels", "href": "#summary-panels"},
                     {"label": "Tabs", "href": "#tabs"},
                 ],
