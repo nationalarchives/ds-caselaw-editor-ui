39f8ad636b672e340e62e4ee94b4fbb4bb608c54
James Harries <dxw@pancakeuprising.com>
2025-07-30 12:29:37 +0100

    FCL-1070 | give envs defaults if nothing provided

--- e2e_tests/utils/authentication.py
+++ e2e_tests/utils/authentication.py
@@ -4,7 +4,7 @@ env = environ.Env()
 
 
 def login_user(page):
-    page.get_by_label("Username").fill(env("E2E_LOGIN_USERNAME"))
-    page.get_by_label("Password").fill(env("E2E_LOGIN_PASSWORD"))
+    page.get_by_label("Username").fill(env("E2E_LOGIN_USERNAME", default="root"))
+    page.get_by_label("Password").fill(env("E2E_LOGIN_PASSWORD", default="password"))
 
     page.get_by_role("button", name="Sign in").click()
