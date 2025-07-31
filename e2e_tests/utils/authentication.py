68b50fef87b2aed0015d02125cd2b42223f3d9c9
James Harries <dxw@pancakeuprising.com>
2025-07-31 02:40:04 +0100

    FCL-1070 | add mid login snapshot to help with debugging

--- e2e_tests/utils/authentication.py
+++ e2e_tests/utils/authentication.py
@@ -1,10 +1,14 @@
 import environ
 
+from .assertions import assert_matches_snapshot
+
 env = environ.Env()
 
 
 def login_user(page):
     page.get_by_label("Username").fill(env("E2E_LOGIN_USERNAME", default="root"))
     page.get_by_label("Password").fill(env("E2E_LOGIN_PASSWORD", default="password"))
+
+    assert_matches_snapshot(page, "login_test")
 
     page.get_by_role("button", name="Sign in").click()
