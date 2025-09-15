b9d27ebf456f04378f19ddb78b00f64921e757ce
Nick Jackson <nick@dxw.com>
2025-09-15 13:05:35 +0100

    Allow deletion of identifiers for unpublished documents

--- judgments/views/document_identifiers.py
+++ judgments/views/document_identifiers.py
@@ -1,9 +1,13 @@
 from typing import TYPE_CHECKING
 
+from caselawclient.models.documents import Document
 from caselawclient.models.identifiers.neutral_citation import NCNValidationException
 from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import Button, Layout
 from django import forms
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.urls import reverse
 from django.views.generic import FormView
 
@@ -109,6 +113,66 @@ class AddDocumentIdentifierView(DocumentViewMixin, FormView):
 
         # Everything checks out - save the identifiers to the database.
         self.document.save_identifiers()
+
+        messages.success(
+            self.request,
+            f'New {identifier_type.schema.name} with value "{identifier_value}" added successfully!',
+        )
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("document-identifiers", kwargs={"document_uri": self.document.uri})
+
+
+class DeleteIdentifierForm(forms.Form):
+    def __init__(self, *args, type_choices=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(Button.warning("submit", "Delete"))
+
+
+def check_safe_to_delete_identifier(document: Document, identifier_uuid: str):
+    if document.has_ever_been_published:
+        msg = f"Document {document.uri} has previously been published; identifiers cannot be deleted."
+        raise PermissionDenied(
+            msg,
+        )
+
+    if identifier_uuid not in document.identifiers:
+        msg = f'Identifier "{identifier_uuid}" does not exist in {document.uri}.'
+        raise Http404(
+            msg,
+        )
+
+
+class DeleteDocumentIdentifierView(DocumentViewMixin, FormView):
+    template_name = "judgment/identifier_delete.html"
+    form_class = DeleteIdentifierForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        check_safe_to_delete_identifier(self.document, self.kwargs["identifier_uuid"])
+
+        context["view"] = "document_identifiers"
+        context["identifier"] = self.document.identifiers[self.kwargs["identifier_uuid"]]
+
+        return context
+
+    def form_valid(self, form):
+        check_safe_to_delete_identifier(self.document, self.kwargs["identifier_uuid"])
+
+        identifier = self.document.identifiers[self.kwargs["identifier_uuid"]]
+
+        # Remove the identifier
+        del self.document.identifiers[self.kwargs["identifier_uuid"]]
+
+        # Save the identifiers to the database. This runs validations as part of save operations, so if we've ended up here but the result is invalid it won't be committed.
+        self.document.save_identifiers()
+
+        messages.success(self.request, f'{identifier.schema.name} with value "{identifier.value}" has been deleted.')
 
         return super().form_valid(form)
 
