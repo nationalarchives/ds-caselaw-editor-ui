a27ebfc4b36faf574c1115f3e3b41be042411344
James Harries <dxw@pancakeuprising.com>
2025-09-18 14:29:03 +0100

    FCL-1296 | add new search attributes

--- judgments/views/index.py
+++ judgments/views/index.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from judgments.utils.view_helpers import get_search_parameters, get_search_results
+from judgments.utils.view_helpers import get_context_with_search_filters, get_search_parameters, get_search_results
 
 
 def index(request):
     query = get_search_parameters(request.GET, only_unpublished=True)
     context = get_search_results(query)
+
+    context = get_context_with_search_filters(context)
+
     template = loader.get_template("pages/home.html")
+
     return HttpResponse(template.render(context, request))
