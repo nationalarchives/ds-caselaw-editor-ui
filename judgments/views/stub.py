9a29741a148d855db0dabc524a02b1ad2eb9cd2a
David McKee <david.mckee@dxw.com>
2025-12-09 16:27:37 +0000

    Create a stub from form fields but don't do anything with it

--- judgments/views/stub.py
+++ judgments/views/stub.py
@@ -1,3 +1,8 @@
+import datetime
+
+from caselawclient.models.documents.stub import EditorStubData
+from caselawclient.models.documents.stub import create_stub as create_rendered_stub
+from django import forms
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
@@ -10,24 +15,67 @@ from judgments.utils.view_helpers import (
 )
 
 
+class StubForm(forms.Form):
+    # Django form for display
+    decision_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}),
+        label="Decision date",
+    )
+    # transform_datetime is dynamically generated
+    court_code_upper = forms.CharField(label="Court code", max_length=100)
+    title = forms.CharField(label="Title", max_length=100)
+    year = forms.IntegerField(label="Year", min_value=1001)
+    case_numbers = forms.CharField(widget=forms.Textarea(attrs={"rows": 4}), label="Case numbers", max_length=100)
+    claimants = forms.CharField(widget=forms.Textarea(attrs={"rows": 4}), label="Claimants", max_length=100)
+    respondants = forms.CharField(widget=forms.Textarea(attrs={"rows": 4}), label="Respondants", max_length=100)
+
+
 class CreateStubView(TemplateView):
     template_name = "judgment/stub.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["view"] = "create_stub"
+        context["form"] = StubForm()
         return context
+
+
+def list_from_string(s):
+    return [line.strip() for line in s.split("\n") if line.strip()]
 
 
 def create_stub(request):
     if not (user_is_superuser(request.user) or user_is_editor(request.user)):
-        msg = "Only superusers and editors can delete documents"
+        msg = "Only superusers and editors can create documents"
         raise PermissionDenied(msg)
 
-    name = request.POST.get("name", None)
+    stub_form = StubForm(request.POST)
+    if not stub_form.is_valid():
+        msg = "Invalid form data"
+        raise RuntimeError(msg)
 
+    case_numbers = list_from_string(stub_form["case_numbers"].value())
+    claimants = list_from_string(stub_form["claimants"].value())
+    respondants = list_from_string(stub_form["respondants"].value())
+    parties = [{"role": "claimant", "name": claimant} for claimant in claimants] + [
+        {"role": "respondant", "name": respondant} for respondant in respondants
+    ]
+
+    stub_data = EditorStubData(
+        {
+            "decision_date": stub_form["decision_date"].value(),
+            "transform_datetime": datetime.datetime.now(tz=datetime.UTC).strftime("%Y-%m-%dT%H:%M:%S"),
+            "court_code_upper": stub_form["court_code_upper"].value().upper(),
+            "title": stub_form["title"].value(),
+            "year": str(stub_form["year"].value()),
+            "case_numbers": case_numbers,
+            "parties": parties,
+        },
+    )
+
+    rendered_stub = create_rendered_stub(stub_data)
     messages.success(
         request,
-        f"The {name} at URI X was successfully X.",
+        f"{rendered_stub}",
     )
     return HttpResponseRedirect(reverse("home"))
