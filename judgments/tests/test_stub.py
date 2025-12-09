71a30d9f3b39292511858964bdd06cb712cb4762
David McKee <david.mckee@dxw.com>
2025-12-09 17:58:05 +0000

    Semifunctional tests

--- judgments/tests/test_stub.py
+++ judgments/tests/test_stub.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import ANY, patch
 
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -13,11 +13,11 @@ class TestStubView(TestCase):
         assert "Case numbers:" in decoded_response
         assert response.status_code == 200
 
-    @patch("judgments.views.stub.messages.success")
-    def test_judgment_stub_post(self, mock_success):
+    @patch("judgments.views.stub.create_rendered_stub", return_value="<xml>")
+    def test_judgment_stub_post(self, mock_render_stub):
         superuser = User.objects.create_superuser(username="clark")
         self.client.force_login(superuser)
-        response = self.client.post(
+        _response = self.client.post(
             "/create_stub",
             {
                 "decision_date": "2024-01-01",
@@ -29,5 +29,24 @@ class TestStubView(TestCase):
                 "respondants": "Cathy\nDarren",
             },
         )
-        mock_success.assert_called_with("ham")
-        return response
+        mock_render_stub.assert_called_with(
+            {
+                "decision_date": "2024-01-01",
+                "transform_datetime": ANY,
+                "court_code_upper": "UKSC",
+                "title": "A title",
+                "year": "2024",
+                "case_numbers": ["ABC123", "DEF123"],
+                "parties": [
+                    {"role": "claimant", "name": "Amy"},
+                    {"role": "claimant", "name": "Barry"},
+                    {"role": "respondant", "name": "Cathy"},
+                    {"role": "respondant", "name": "Darren"},
+                ],
+            },
+        )
+
+        # date has a T in the right place and is long enough to have seconds
+        transform = mock_render_stub.call_args.args[0]["transform_datetime"]
+        assert transform[10] == "T"
+        assert len(transform) == 19
