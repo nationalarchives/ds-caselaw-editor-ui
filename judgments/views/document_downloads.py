7a9f13aab776ae2414d9f7dbddb34dca3481b471
James Harries <dxw@pancakeuprising.com>
2026-04-23 13:30:43 +0100

    FCLC-1825 | migrate download page to jinja

--- judgments/views/document_downloads.py
+++ judgments/views/document_downloads.py
@@ -2,7 +2,8 @@ from judgments.utils.view_helpers import DocumentView
 
 
 class DocumentDownloadsView(DocumentView):
-    template_name = "judgment/downloads.html"
+    template_engine = "jinja"
+    template_name = "judgment/downloads.jinja"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
