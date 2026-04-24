4754dde44f2423025aa31ef9bfabee52f23c6bb6
James Harries <dxw@pancakeuprising.com>
2026-04-24 11:47:41 +0100

    FCLC-1825 | migrate unhold  page to jinja

--- judgments/views/judgment_hold.py
+++ judgments/views/judgment_hold.py
@@ -43,7 +43,8 @@ def hold(request):
 
 
 class UnholdDocumentView(DocumentView):
-    template_name = "judgment/unhold.html"
+    template_engine = "jinja"
+    template_name = "judgment/unhold.jinja"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
