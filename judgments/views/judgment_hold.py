e0994720448cee380513cb2308f02e73d3a4c512
James Harries <dxw@pancakeuprising.com>
2026-04-24 11:41:31 +0100

    FCLC-1825 | migrate hold success page to jinja

--- judgments/views/judgment_hold.py
+++ judgments/views/judgment_hold.py
@@ -18,7 +18,8 @@ class HoldDocumentView(DocumentView):
 
 
 class HoldDocumentSuccessView(DocumentView):
-    template_name = "judgment/hold-success.html"
+    template_engine = "jinja"
+    template_name = "judgment/hold-success.jinja"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
