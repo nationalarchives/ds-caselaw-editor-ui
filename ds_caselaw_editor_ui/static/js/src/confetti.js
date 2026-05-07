33f3f5665d56358186551a17c3b8f28917f8e777
James Harries <dxw@pancakeuprising.com>
2026-05-07 13:38:40 +0100

    FCLC-1838 | move confetti dependency to npm

---
+++ ds_caselaw_editor_ui/static/js/src/confetti.js
@@ -0,0 +1,70 @@
+import { tsParticles } from "@tsparticles/engine";
+import { loadConfettiPreset } from "@tsparticles/preset-confetti";
+
+(async () => {
+  await loadConfettiPreset(tsParticles);
+
+  await tsParticles.load({
+    id: "tsparticles",
+    options: {
+      preset: "confetti",
+
+      particles: {
+        color: {
+          value: ["#CB0D07", "#EBAB00", "#FCE45C", "#008484", "#405480"],
+        },
+      },
+
+      emitters: [
+        {
+          startCount: 50,
+          position: {
+            x: 0,
+            y: 60,
+          },
+          rate: {
+            delay: 0,
+            quantity: 0,
+          },
+          life: {
+            duration: 0.1,
+            count: 1,
+          },
+          particles: {
+            move: {
+              direction: "top-right",
+              speed: {
+                min: 50,
+                max: 150,
+              },
+            },
+          },
+        },
+        {
+          startCount: 50,
+          position: {
+            x: 100,
+            y: 60,
+          },
+          rate: {
+            delay: 0,
+            quantity: 0,
+          },
+          life: {
+            duration: 0.1,
+            count: 1,
+          },
+          particles: {
+            move: {
+              direction: "top-left",
+              speed: {
+                min: 50,
+                max: 150,
+              },
+            },
+          },
+        },
+      ],
+    },
+  });
+})();
