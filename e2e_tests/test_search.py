acfa8b41aa0764d43a537cf6f06f8de37312f7bd
James Harries <dxw@pancakeuprising.com>
2026-07-06 13:35:03 +0100

    FCLC-2090 | add cassette for search results

--- e2e_tests/test_search.py
+++ e2e_tests/test_search.py
@@ -13,8 +13,9 @@ def test_search(authenticated_page: Page):
     authenticated_page.get_by_label("NCN").click()
     expect(authenticated_page.get_by_label("NCN")).to_be_checked()
     expect(authenticated_page.get_by_label("Everywhere")).not_to_be_checked()
-    assert_matches_snapshot(authenticated_page, "search_results_page")
 
-    authenticated_page.get_by_role("searchbox").fill("something")
+    authenticated_page.get_by_label("Everywhere").click()
+    authenticated_page.get_by_role("searchbox").fill("Imperial")
     authenticated_page.get_by_role("button", name="Search").click()
     expect(authenticated_page).to_have_url(re.compile(r"/results"))
+    assert_matches_snapshot(authenticated_page, "search_results_page")
