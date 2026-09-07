44b9bcbc88fa5eaa302ccf8ac22baab81a733f2a
James Harries <dxw@pancakeuprising.com>
2026-09-07 09:34:21 +0100

    FCLC-2259 | remove deprecated cookie banner

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ module.exports = {
   entry: {
     app: "./ds_caselaw_editor_ui/static/js/src/app.js",
     confetti: "./ds_caselaw_editor_ui/static/js/src/confetti.js",
-    cookie_consent:
-      "./ds_caselaw_editor_ui/static/js/cookie_consent/src/ds-cookie-consent.js",
     gtm_script: "./ds_caselaw_editor_ui/static/js/src/gtm_script.js",
   },
   output: {
